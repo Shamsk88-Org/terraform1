7aee82dc32e08a134d5ea374e00a09e441f95575
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-13 17:48:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+  client_id = "10184fbc-168b-423f-a39e-6d5c5a87bbd3"
+  client_secret = "z5M8Q~Zc2J~MVO2psVLuzI4UzXDh_QI3.Iyh3cfx"
+  tenant_id = "648b628e-c17f-4d43-8193-72e3f4d7499d"
+  subscription_id = "24143a81-fcba-4b21-9b28-c910b75e6580"
+}
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-dev-app-001"
@@ -19,6 +27,6 @@ terraform {
 }
 
 resource "azurerm_resource_group" "test" {
-  name     = "rg-dev-app-003"
+  name     = "rg-dev-app-005"
   location = "North Europe"
 }
