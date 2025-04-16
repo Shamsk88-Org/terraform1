d3913d184d80550d2a9fa41d9b4eb67692be11a1
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-16 23:28:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-dev-app-001"
-    storage_account_name = "proddevops2025"
-    container_name       = "production"
-    key                  = "prodtfstate.tfstate"
-    tenant_id            = "648b628e-c17f-4d43-8193-72e3f4d7499d"
-    subscription_id      = "24143a81-fcba-4b21-9b28-c910b75e6580"
-  }
-}
-
 resource "azurerm_resource_group" "test" {
   name     = var.resource_name
   location = var.location
