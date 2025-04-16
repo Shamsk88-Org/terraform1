cada56827aa41af09a21d68c020ca336f25ed96b
Shamsher Khan <shamsk88@gmail.com>
2025-04-16 23:25:44 +0530

    updated code

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-dev-app-001"
+    storage_account_name = "proddevops2025"
+    container_name       = "production"
+    key                  = "${var.resource_group_name}.tfstate"
+   }
+}
