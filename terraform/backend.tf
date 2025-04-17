f2d87957dff04e7898a2cbaa114ebb51ff05299f
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-17 14:22:08 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name = "rg-dev-app-001"
     storage_account_name = "proddevops2025"
     container_name = "production"
-    key = local.tfstate_key
+    key = locals.tfstate_key
    }
 }
