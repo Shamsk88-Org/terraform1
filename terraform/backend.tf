f5d737d8384b7d1f23077d9fa094dc8e0646a86f
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-17 14:47:27 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name = "rg-dev-app-001"
-    storage_account_name = "proddevops2025"
-    container_name = "production"
-    key = locals.tfstate_key
-   }
+  backend "azurerm" {}
 }
