976918ca6bdcd4499fbdb12febf6033e727696cf
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-17 19:20:44 +0530

    Update read_tfvars.sh

--- .github/workflows/read_tfvars.sh
+++ .github/workflows/read_tfvars.sh
@@ -1,4 +1,4 @@
 # read_tfvars.sh
 #!/bin/bash
-value=$(grep 'resource_name' /home/runner/work/terraform1/terraform1/terraform/terraform.tfvars | awk -F'=' '{print $3}' | tr -d ' "')
+value=$(grep 'resource_name' /home/runner/work/terraform1/terraform1/terraform/terraform.tfvars | awk -F'=' '{print $2}' | tr -d ' "')
 echo $value
