19e0d68bec5d1e8c2a508ac797b8a50d4244f3f4
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-05-07 16:02:17 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -6,14 +6,6 @@ variable "subscription_id" {
 description = "The value of the Subscription ID"
 }
 
-variable "client_id" {
-description = "The value of the Client ID"
-}
-
-variable "client_secret" {
-description = "The value of the Client secret"
-}
-
 variable "resource_name" {
 description = "The name of the resource"
 }
